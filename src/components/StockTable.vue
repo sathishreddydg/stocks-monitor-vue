bbfcf42316a137c4c2488a3e79a7805ced985a11
sdulu <sathish.duluganti@monsanto.com>
2018-12-05 16:10:50 -0600

    Setup basic table for for live feed

--- src/components/StockTable.vue
+++ src/components/StockTable.vue
@@ -1,29 +1,45 @@
 <template>
   <div>
-
+<v-data-table
+    :headers="headers"
+    :items="stockData"
+    class="elevation-1"
+  >
+    <template slot="items" slot-scope="props">
+      <td>{{ props.item.symbol }}</td>
+      <td>{{ props.item.price }}</td>
+      <td>{{ props.item.volume }}</td>
+      <td>{{ props.item.bid }}</td>
+      <td>{{ props.item.ask }}</td>
+      <td>{{ props.item.week52 }}</td>
+      <td>{{ props.item.todayRange }}</td>
+    </template>
+  </v-data-table>
   </div>
 </template>
 
 <script>
 import io from 'socket.io-client'
-let socket = io('https://ws-api.iextrading.com/1.0/deep')
+let socket = io('https://ws-api.iextrading.com/1.0/tops')
 
 export default {
   data: function () {
     return {
       headers: [
         {
-          text: 'Ticker',
+          text: 'Symobl',
           align: 'left',
-          value: 'ticker'
+          value: 'symbol',
+          class: 'tbl-column'
         },
-        { text: 'Quote', value: 'quote' },
+        { text: 'Price', value: 'price' },
         { text: 'Volume', value: 'volume' },
         { text: 'Bid', value: 'bid' },
         { text: 'Ask', value: 'ask' },
         { text: '52 Week', value: '52week' },
         { text: 'Today range', value: 'todayRange' }
-      ]
+      ],
+      stockData: []
     }
   },
 
@@ -34,12 +50,27 @@ export default {
   },
 
   mounted: function () {
-    socket.on('message', data => console.log(data))
+    socket.on('message', data => {
+      data = JSON.parse(data)
+      const index = this.stockData.findIndex(stock => stock.symbol === data.symbol)
+      if (index > -1) {
+        this.stockData[index].price = data.lastSalePrice
+        this.stockData[index].bid = data.bidPrice + ' x ' + data.bidSize
+        this.stockData[index].ask = data.askPrice + ' x ' + data.askSize
+      } else {
+        this.stockData.push({
+          symbol: data.symbol,
+          price: data.lastSalePrice,
+          volume: 'FromAPI',
+          bid: data.bidPrice + ' x ' + data.bidSize,
+          ask: data.askPrice + ' x ' + data.askSize,
+          week52: 'FromAPI',
+          todayRange: 'From API'
+        })
+      }
+    })
     socket.on('connect', () => {
-      socket.emit('subscribe', JSON.stringify({
-        symbols: ['aapl'],
-        channels: ['deep']
-      }))
+      socket.emit('subscribe', 'aapl')
     })
   }
 }
@@ -47,4 +78,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+.tbl-column{
+  width: 100px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import '@babel/polyfill'
 import Vue from 'vue'
+import axios from 'axios'
 import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
@@ -9,6 +10,8 @@ import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 Vue.config.productionTip = false
+
+Vue.prototype.$http = axios
 
 new Vue({
   router,
