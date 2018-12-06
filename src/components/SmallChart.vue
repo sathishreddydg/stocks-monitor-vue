52e7e221ccb6dfc6ec14cf8e934d19d56321ecb8
sdulu <sathish.duluganti@monsanto.com>
2018-12-06 17:12:36 -0600

    Multiple charts with symbols array

--- src/components/SmallChart.vue
+++ src/components/SmallChart.vue
@@ -1,17 +1,27 @@
 <template>
-<div id="container" style="width:100%; height:400px;"></div>
+<div>
+  <span>{{symbol}}</span>
+<div :id="symbol"></div>
+</div>
 </template>
 
 <script>
 export default {
+  props: ['symbol'],
   data: function () {
     return {}
   },
   methods: {
     createChart: function () {
-      this.$http.get('https://api.iextrading.com/1.0/stock/aapl/chart/1d').then(({ data }) => {
-        data = data.map(quote => quote.close)
-        this.$chart.chart('container', {
+      this.$http.get(`https://api.iextrading.com/1.0/stock/${this.symbol}/chart/1d`).then(({ data }) => {
+        let lastGoodValue
+        data = data.map((quote, index) => {
+          if (quote.marketClose || quote.close) {
+            lastGoodValue = quote.marketClose || quote.close
+          }
+          return lastGoodValue
+        })
+        this.$chart.chart(this.symbol, {
           title: {
             text: ''
           },
@@ -22,10 +32,14 @@ export default {
             visible: false
           },
           yAxis: {
-            visible: false
+            visible: false,
+            height: 200
           },
           chart: {
-            backgroundColor: 'rgab(0,0,0,1)'
+            margin: 0,
+            backgroundColor: 'rgab(1,1,1,0)',
+            height: 200,
+            width: 450
           },
           credits: {
             enabled: false
@@ -39,7 +53,7 @@ export default {
             }
           },
           series: [{
-            name: 'Area',
+            name: this.symbol,
             type: 'area',
             data: data,
             pointStart: Date.UTC(2011, 0),
@@ -54,22 +68,20 @@ export default {
                 enabled: false
               },
               threshold: data[0],
-              fillColor: {
-                linearGradient: [0, 0, 0, 240],
-                stops: [
-                  [0, 'rgba(0,255,0,1)'],
-                  [1, 'rgba(0,255,0,0)']
-                ]
-              },
-              negativeFillColor: {
-                linearGradient: [0, 0, 0, 350],
-                stops: [
-                  [0.1, 'rgba(255,0,0,0)'],
-                  [1, 'rgba(255,0,0,1)']
-                ]
-              }
+              softThreshold: false,
+              fillColor: 'rgba(0,255,0,0.2)',
+              negativeFillColor: 'rgba(255,0,0,0.2)'
             }
           }
+        }, chart => {
+          var ext = chart.yAxis[0].getExtremes()
+          console.log(chart.yAxis[0])
+          var dMax = Math.abs(ext.dataMax)
+          var dMin = Math.abs(ext.dataMin)
+          var dExt = dMax >= dMin ? dMax : dMin
+          var min = 2 * chart.yAxis[0].threshold - dExt
+          console.log(min, chart.yAxis[0].threshold, dExt)
+          chart.yAxis[0].setExtremes(min, dExt)
         })
       }).catch(err => console.error(err))
     }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,21 @@
 <template>
   <div class="home">
     <stock-table/>
-    <small-chart></small-chart>
+    <v-layout>
+      <v-flex>
+        <v-card>
+          <v-container v-bind="{ [`grid-list-md`]: true }" fluid>
+            <v-layout row wrap>
+              <v-flex v-for="symbol in symbols" :key="symbol" xs3>
+                <v-card flat tile>
+                  <small-chart :symbol='symbol'></small-chart>
+                </v-card>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card>
+      </v-flex>
+    </v-layout>
   </div>
 </template>
 
@@ -14,6 +28,21 @@ export default {
   components: {
     stockTable,
     smallChart
+  },
+  data: function () {
+    return {
+      symbols: []
+    }
+  },
+  methods: {
+    getActiveSymbols: function () {
+      this.$http.get('https://api.iextrading.com/1.0/stock/market/list/gainers').then(({ data }) => {
+        this.symbols = ['fb', 'tsla']// ['aapl','msft','fb','tsla','amd','amzn','goog','atvi','shop']//data.map(stock=>stock.symbol)
+      }).catch(err => console.error(err))
+    }
+  },
+  created: function () {
+    this.getActiveSymbols()
   }
 }
 </script>
