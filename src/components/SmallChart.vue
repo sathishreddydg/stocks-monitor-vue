a470837fc302be046ac03beaee2b7a8bca913838
sdulu <sathish.duluganti@monsanto.com>
2019-01-02 14:16:26 -0600

    Add basic batch quote and chart along with news query

--- src/components/SmallChart.vue
+++ src/components/SmallChart.vue
@@ -1,92 +1,90 @@
 <template>
 <div>
-  <span>{{symbol}}</span>
-<div :id="symbol"></div>
+<div :id="chartData.symbol"></div>
 </div>
 </template>
 
 <script>
 export default {
-  props: ['symbol'],
+  props: ['chartData'],
   data: function () {
     return {}
   },
   methods: {
     createChart: function () {
-      this.$http.get(`https://api.iextrading.com/1.0/stock/${this.symbol}/chart/1d`).then(({ data }) => {
-        let lastGoodValue
-        data = data.map((quote, index) => {
-          if (quote.marketClose || quote.close) {
-            lastGoodValue = quote.marketClose || quote.close
+      let lastGoodValue
+      let data = this.chartData.chart
+      data = this.chartData.chart.map((quote, index) => {
+        if (quote.marketClose || quote.close) {
+          lastGoodValue = quote.marketClose || quote.close
+        }
+        for (let i = 0; i < data.length; i++) {
+          if (!lastGoodValue) {
+            lastGoodValue = data[index + i].marketClose || data[index + i].close
+          } else {
+            break
           }
-          for (let i = 0; i < data.length; i++) {
-            if (!lastGoodValue) {
-              lastGoodValue = data[index + i].marketClose || data[index + i].close
-            } else {
-              break
-            }
+        }
+        return lastGoodValue
+      })
+      this.$chart.chart(this.chartData.symbol, {
+        title: {
+          text: ''
+        },
+        subtitle: {
+          text: ''
+        },
+        xAxis: {
+          visible: false
+        },
+        yAxis: {
+          visible: false,
+          height: 200,
+          tickPositioner: function () {
+            var maxDeviation = Math.max(this.dataMax - this.threshold, this.threshold - this.dataMin)
+            var halfMaxDeviation = maxDeviation / 2
+            return [this.threshold - maxDeviation, this.threshold - halfMaxDeviation, this.threshold, this.threshold + halfMaxDeviation, this.threshold + maxDeviation]
           }
-          return lastGoodValue
-        })
-        this.$chart.chart(this.symbol, {
-          title: {
-            text: ''
-          },
-          subtitle: {
-            text: ''
-          },
-          xAxis: {
-            visible: false
-          },
-          yAxis: {
-            visible: false,
-            height: 200,
-            tickPositioner: function () {
-              var maxDeviation = Math.max(this.dataMax - this.threshold, this.threshold - this.dataMin)
-              var halfMaxDeviation = maxDeviation / 2
-              return [this.threshold - maxDeviation, this.threshold - halfMaxDeviation, this.threshold, this.threshold + halfMaxDeviation, this.threshold + maxDeviation]
-            }
-          },
-          chart: {
-            margin: 0,
-            backgroundColor: 'rgab(1,1,1,0)',
-            height: 200
-          },
-          credits: {
+        },
+        chart: {
+          margin: 0,
+          backgroundColor: 'rgab(1,1,1,0)',
+          height: 200
+        },
+        credits: {
+          enabled: false
+        },
+        legend: {
+          enabled: false
+        },
+        navigation: {
+          buttonOptions: {
             enabled: false
-          },
-          legend: {
-            enabled: false
-          },
-          navigation: {
-            buttonOptions: {
+          }
+        },
+        series: [{
+          name: this.chartData.symbol,
+          type: 'area',
+          data: data,
+          // TODO: make it time
+          pointStart: Date.UTC(2011, 0),
+          pointInterval: 30 * 24 * 36e5,
+          lineWidth: 1,
+          color: '#0f0',
+          negativeColor: '#f00'
+        }],
+        plotOptions: {
+          series: {
+            marker: {
               enabled: false
-            }
-          },
-          series: [{
-            name: this.symbol,
-            type: 'area',
-            data: data,
-            // TODO: make it time
-            pointStart: Date.UTC(2011, 0),
-            pointInterval: 30 * 24 * 36e5,
-            lineWidth: 1,
-            color: '#0f0',
-            negativeColor: '#f00'
-          }],
-          plotOptions: {
-            series: {
-              marker: {
-                enabled: false
-              },
-              threshold: data[0],
-              softThreshold: false,
-              fillColor: 'rgba(0,255,0,0.2)',
-              negativeFillColor: 'rgba(255,0,0,0.2)'
-            }
+            },
+            threshold: this.chartData.previousClose,
+            softThreshold: false,
+            fillColor: 'rgba(0,255,0,0.2)',
+            negativeFillColor: 'rgba(255,0,0,0.2)'
           }
-        })
-      }).catch(err => console.error(err))
+        }
+      })
     }
   },
   mounted: function () {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,9 +3,10 @@
     <!-- <stock-table/> -->
     <v-container grid-list-md ml-0 mr-0>
       <v-layout row wrap>
-        <v-flex v-for="symbol in symbols" :key="symbol" xs3 sm3>
+        <v-flex v-for="stock in stockData" :key="stock.quote.symbol" xs6 sm6>
           <v-card>
-            <small-chart :symbol='symbol'></small-chart>
+            <span>{{stock.quote.symbol}} {{stock.quote.currentPrice}} {{stock.quote.percent}}</span>
+            <small-chart :chart-data='stock.chartData'></small-chart>
           </v-card>
         </v-flex>
       </v-layout>
@@ -25,19 +26,57 @@ export default {
   },
   data: function () {
     return {
-      symbols: []
+      symbols: [],
+      chartRange: '1d',
+      stockData: []
     }
   },
+
+  computed: {
+    canItBatch: function () {
+      return (new Date()).toISOString().split('T')[0].split('-').join('') === this.chartRange
+    }
+  },
+
   methods: {
-    getActiveSymbols: function () {
-      this.$http.get('https://api.iextrading.com/1.0/stock/market/list/gainers').then(({ data }) => {
-        // this.symbols =  ['aapl','msft','fb','tsla','amd','amzn','goog','atvi','shop']
-        this.symbols = data.map(stock => stock.symbol)
+    // TODO: useful on market closing days
+    setDefaultChartDate: function () {
+      this.$http.get('https://api.iextrading.com/1.0/stock/aapl/quote').then(({ data }) => {
+        this.chartRange = (new Date(data.closeTime)).toISOString().split('T')[0].split('-').join('')
       }).catch(err => console.error(err))
+    },
+
+    initialize: function () {
+      this.symbols = ['aapl', 'msft', 'fb', 'tsla', 'amd', 'amzn', 'goog', 'atvi', 'shop']
+      this.$http.get('https://api.iextrading.com/1.0/stock/market/list/gainers').then(({ data }) => {
+        this.symbols = this.symbols.concat(data.map(stock => stock.symbol))
+        return this.$http.get('https://api.iextrading.com/1.0/stock/market/batch?symbols=' + this.symbols.join(',') + '&types=quote,news,chart&range=1d')
+      }).then(({ data }) => {
+        this.stockData = Object.keys(data).map((key) => {
+          return {
+            quote: {
+              symbol: data[key].quote.symbol,
+              currentPrice: data[key].quote.latestPrice,
+              percent: this.getPercent(data[key].quote.latestPrice, data[key].quote.previousClose)
+            },
+            chartData: {
+              symbol: data[key].quote.symbol,
+              previousClose: data[key].quote.close,
+              chart: data[key].chart
+            }
+          }
+        })
+        console.log(this.stockData)
+      }).catch(err => console.error(err))
+    },
+
+    getPercent: function (current, previous) {
+      return parseFloat(Math.round(((current - previous) / previous) * 100 * 100) / 100).toFixed(2)
     }
   },
   created: function () {
-    this.getActiveSymbols()
+    this.setDefaultChartDate()
+    this.initialize()
   }
 }
 </script>
